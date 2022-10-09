6cb1a392a7144835ea2a59812d24b79e997fc282
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-10-09 09:47:14 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<T,Y>
+    {
+        KeyValuePair<T, Y>[] items;
+        public MyDictionary()
+        {
+            items = new KeyValuePair<T, Y>[0];
+        }
+
+        public int Count { get {return items.Length; } }
+        public void Add(T key,Y value)
+        {
+            KeyValuePair<T, Y>[] tempArray = items;
+            items = new KeyValuePair<T, Y>[items.Length+1];
+
+            for (int i = 0; i < tempArray.Length; i++) 
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = new KeyValuePair<T, Y>(key,value); 
+        }
+
+        public void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key +"-"+item.Value);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary;
+
+Console.WriteLine("Hello, World!");
+
+MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+myDictionary.Add(1, "Ferat EFİL");
+myDictionary.Add(2, "Bahadır EFİL");
+
+Console.WriteLine("Eklenen Eleman Sayısı : "+ myDictionary.Count);
+
+Console.WriteLine("Eklenen Elemanlar");
+
+myDictionary.ShowList();
